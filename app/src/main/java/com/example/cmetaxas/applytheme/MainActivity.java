ce551bf711b8a56dcae3a87a44c2adfb9879b4d3
cmetaxas <@.com>
2016-09-06 15:17:51 +0300

    .

--- app/src/main/java/com/example/cmetaxas/applytheme/MainActivity.java
+++ app/src/main/java/com/example/cmetaxas/applytheme/MainActivity.java
@@ -4,6 +4,9 @@ import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,12 +23,50 @@ public class MainActivity extends AppCompatActivity {
         getSupportActionBar().setTitle("Welcome !");
         toolbar.setSubtitle("Folks !");
 
-        
+//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+//            // With material design APIs
+//            toolbar.setElevation(10f);
+//        } else {
+//            // Without material
+//        }
+    }
 
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            // With material design APIs
-        } else {
-            // Without material
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        String msg = "";
+
+        switch (item.getItemId()) {
+
+            case R.id.discard:
+                msg = getString(R.string.delete);
+                break;
+
+            case R.id.search:
+                msg = getString(R.string.search);
+                break;
+
+            case R.id.edit:
+                msg = getString(R.string.edit);
+                break;
+
+            case R.id.settings:
+                msg = getString(R.string.settings);
+                break;
+
+            case R.id.Exit:
+                msg = getString(R.string.exit);
+                break;
         }
+
+        Toast.makeText(this, msg + " clicked !", Toast.LENGTH_SHORT).show();
+
+        return super.onOptionsItemSelected(item);
     }
 }
